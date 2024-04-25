9848472a8ed59ee1a8b010a6c3edf40201ecd22c
Al-Amin <116742755+adnanalamin@users.noreply.github.com>
2024-04-25 23:28:20 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const cors = require('cors');
+require("dotenv").config();
+
+const app = express()
+
+
+
+const port = process.env.PORT || 5000
+
+
+
+app.use(cors())
+app.use(express.json())
+
+
+
+app.get('/', (req, res) => {
+    res.send('Server is Running')
+})
+
+app.listen(port, () => {
+    console.log('Server Site is Running')
+})
