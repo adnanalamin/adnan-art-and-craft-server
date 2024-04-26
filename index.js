1f3a535576083fa39f24744ff3d58778da5040fc
Al-Amin <116742755+adnanalamin@users.noreply.github.com>
2024-04-27 02:09:36 +0600

    Find One data usign id

--- index.js
+++ index.js
@@ -1,65 +1,63 @@
-const express = require('express');
-const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const express = require("express");
+const cors = require("cors");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 require("dotenv").config();
 
-const app = express()
+const app = express();
 
+const port = process.env.PORT || 5000;
 
-
-const port = process.env.PORT || 5000
-
-
-
-app.use(cors())
-app.use(express.json())
-
-
+app.use(cors());
+app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.giatfyq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
-
 
 const client = new MongoClient(uri, {
   serverApi: {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
   try {
-    
     await client.connect();
 
     const craftItems = client.db("artAndCraft").collection("Craft");
 
     app.get("/artandcraft", async (req, res) => {
-        const cursor = craftItems.find();
-        const result = await cursor.toArray();
-        res.send(result);
-      });
+      const cursor = craftItems.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    });
 
+    app.get("/artandcraft/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await craftItems.findOne(query);
+      res.send(result);
+    });
 
     app.post("/artandcraft", async (req, res) => {
-        const newCraft = req.body;
-        const result = await craftItems.insertOne(newCraft);
-        res.send(result);
-      });
-
+      const newCraft = req.body;
+      const result = await craftItems.insertOne(newCraft);
+      res.send(result);
+    });
 
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
   }
 }
 run().catch(console.dir);
 
-
-app.get('/', (req, res) => {
-    res.send('Server is Running')
-})
+app.get("/", (req, res) => {
+  res.send("Server is Running");
+});
 
 app.listen(port, () => {
-    console.log('Server Site is Running')
-})
+  console.log("Server Site is Running");
+});
