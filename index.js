410872e694b39632a78fe65f61062630e65e9aa5
Al-Amin <116742755+adnanalamin@users.noreply.github.com>
2024-04-27 01:19:21 +0600

    Get artandCraft all items form database

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ app.use(express.json())
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.giatfyq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+
 const client = new MongoClient(uri, {
   serverApi: {
     version: ServerApiVersion.v1,
@@ -29,10 +29,16 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-    // Connect the client to the server	(optional starting in v4.7)
+    
     await client.connect();
 
     const craftItems = client.db("artAndCraft").collection("Craft");
+
+    app.get("/artandcraft", async (req, res) => {
+        const cursor = craftItems.find();
+        const result = await cursor.toArray();
+        res.send(result);
+      });
 
 
     app.post("/artandcraft", async (req, res) => {
@@ -42,12 +48,9 @@ async function run() {
       });
 
 
-    // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
-    // Ensures that the client will close when you finish/error
-    // await client.close();
   }
 }
 run().catch(console.dir);
